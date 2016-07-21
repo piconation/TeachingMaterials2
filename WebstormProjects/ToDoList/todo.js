ad6501111721d9d3e12a8f69fc0cc4fb09acf813
mattpowell <piconation@yahoo.com>
2016-07-21 10:18:39 -0600

    7.21.16

--- WebstormProjects/ToDoList/todo.js
+++ WebstormProjects/ToDoList/todo.js
@@ -2,22 +2,24 @@
  * Created by mattpowell on 7/18/16.
  */
 
-function Todo($scope) {
+// function Todo($scope) {
+//
+//     $scope.
+//
+//     $scope.getTotalTodos = function () {
+//         return $scope.todos.length;
+//     };
+//
+//     $scope.addTodo = function () {
+//         $scope.todos.push({text:$scope.formTodoText, done:false});
+//         $scope.formTodoText = '';
+//     };
+//
+//         $scope.clearCompleted = function () {
+//             $scope.todos = _.filter($scope.todos, function (todo) {
+//                 return !todo.done;
+//             });
+//         };
+// }
 
-    $scope.
 
-    $scope.getTotalTodos = function () {
-        return $scope.todos.length;
-    };
-
-    $scope.addTodo = function () {
-        $scope.todos.push({text:$scope.formTodoText, done:false});
-        $scope.formTodoText = '';
-    };
-
-        $scope.clearCompleted = function () {
-            $scope.todos = _.filter($scope.todos, function (todo) {
-                return !todo.done;
-            });
-        };
-}
