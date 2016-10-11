1f988093ba6185a8fdc3305200224b3a1017722a
mattpowell <piconation@yahoo.com>
2016-10-11 09:51:27 -0600

    MP

--- WebstormProjects/Node/test.js
+++ WebstormProjects/Node/test.js
@@ -2,9 +2,13 @@
  * Created by mattpowell on 10/11/16.
  */
 
-console.log("Hello world");
+var http = require("http");
 
-var module = require("./myfirstmodule");
+var server = http.createServer(function (req, res) {
+    res.end("It works. Hello to " + req.url)
+});
 
-console.log("Done");
+server.listen(8000, function() {
+    console.log("Listening on 8000");
+});
 
