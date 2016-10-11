6a97864ae709faf26ca50f1671f3e91b7de2f50a
mattpowell <piconation@yahoo.com>
2016-10-11 09:09:34 -0600

    MP

---
+++ WebstormProjects/Node/myfirstmodule.js
@@ -0,0 +1,8 @@
+
+module.export = {
+    toUpper: toUpper
+};
+
+function toUpper(value) {
+    return value.toUpperCase();
+}
---
+++ WebstormProjects/Node/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mattpowell on 10/11/16.
+ */
+
+console.log("Hello world");
+
+var module = require("./myfirstmodule");
+
+console.log("Done");
+
