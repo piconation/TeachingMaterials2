05e4b36aaada000815249509f1d4d1d25cdaaff3
mattpowell <piconation@yahoo.com>
2016-11-15 10:28:35 -0700

    1

--- WebstormProjects/NewMatcGP/matc-gp/www/js/server.js
+++ WebstormProjects/NewMatcGP/matc-gp/www/js/server.js
@@ -1,13 +1,17 @@
 // set up ======================================================================
 var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port  	 = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
+var app      = express(); 								        // create our app w/ express
+var mongoose = require('mongoose'); 					    // mongoose for mongodb
+var port  	 = process.env.PORT || 8080; 				  // set the port
+var database = require('./config/database'); 		  // load the database config
+var passport = require('passport');               // loads passport
 
-var morgan = require('morgan'); 		// log requests to the console (express4)
-var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var morgan = require('morgan'); 		              // log requests to the console (express4)
+var bodyParser = require('body-parser'); 	        // pull information from HTML POST (express4)
+var methodOverride = require('method-override');  // simulate DELETE and PUT (express4)
+var OAuthStrategy = require('passport-oauth').OAuthStrategy;
+var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 // configuration ===============================================================
 mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
@@ -25,3 +29,72 @@ require('./app/routes.js')(app);
 // listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
+
+app.post('/login',
+  passport.authenticate('local', {successRedirect: '/app/homebase',
+                                  failureRedirect: '/login'}));
+
+app.configure(function() {
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+});
+
+passport.use('provider', new OAuthStrategy({
+    requestTokenURL: 'https://www.provider.com/oauth/request_token',
+    accessTokenURL: 'https://www.provider.com/oauth/access_token',
+    userAuthorizationURL: 'https://www.provider.com/oauth/authorize',
+    consumerKey: '123-456-789',
+    consumerSecret: 'shhh-its-a-secret'
+    callbackURL: 'https://www.example.com/auth/provider/callback'
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate('Insert user name here', function(err, user) {
+      done(err, user);
+    });
+  }
+));
+
+app.get('/auth/provider/callback',
+  passport.authenticate('provider', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://www.example.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate('Insert user name here', function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+    failureRedirect: '/login' }));
+
+passport.use(new GoogleStrategy({
+    consumerKey: GOOGLE_CONSUMER_KEY,
+    consumerSecret: GOOGLE_CONSUMER_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
